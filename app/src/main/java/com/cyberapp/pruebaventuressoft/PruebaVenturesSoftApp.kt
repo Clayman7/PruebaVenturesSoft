e9cefd6adeaac81d1fe3921df9a84ec089fae773
Clayman7 <clayman.asg@gmail.com>
2024-08-09 13:31:23 -0600

    finalizacion

--- app/src/main/java/com/cyberapp/pruebaventuressoft/PruebaVenturesSoftApp.kt
+++ app/src/main/java/com/cyberapp/pruebaventuressoft/PruebaVenturesSoftApp.kt
@@ -4,5 +4,4 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class PruebaVenturesSoftApp: Application(){
-}
+class PruebaVenturesSoftApp: Application(){}
